55a29feb8d010e4da97b5a46d6ede1f2fc8f9c78
Ayrat Sharipov <sharipov.airat@gmail.com>
2022-04-28 10:32:42 +0300

    feature: kvserver almost done

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,14 @@ import HTTP.HttpTaskServer;
 import manager.FileBackedTasksManager;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        FileBackedTasksManager manager = FileBackedTasksManager.loadFromFile(new File("memoryFile.csv"));
-  HttpTaskServer server = new HttpTaskServer(manager);
-        server.start();
+    public static void main(String[] args) throws IOException {
+   //     FileBackedTasksManager manager = FileBackedTasksManager.loadFromFile(new File("memoryFile.csv"));
+ // HttpTaskServer server = new HttpTaskServer(manager);
+    //    server.start();
+        new KVServer().start();
     }
 }
 
